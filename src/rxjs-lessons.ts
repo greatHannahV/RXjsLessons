d74218f7d87a4bfe277de2f5f325da0947792af3
greatHannahV <i.kasl@mail.ru>
2024-09-08 20:35:37 +0300

    test

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,20 +1,28 @@
-import { subscribe } from 'diagnostics_channel';
-import { Observable } from 'rxjs';
+import { EventDispatcher } from '@angular/core/primitives/event-dispatch';
+import { fromEvent, Observable } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
-const search$ = new Observable<string>((observer) => {
-  console.log('Start');
-  const search = document.getElementById('search');
-  search?.addEventListener('input', (event) => {
-    const target = event.target as HTMLInputElement;
-    observer.next(target.value);
+// const search$ = new Observable<Event>((observer) => {
+//   console.log('Start');
+//   const search = document.getElementById('search');
+//   if (!search) return;
+//   search.addEventListener('input', (event) => {
+//     observer.next(event);
+//   });
+// });
+const search$ = fromEvent(
+  document.getElementById('search') as HTMLInputElement,
+  'input'
+);
+search$
+  .pipe(
+    map((event) => (event.target as HTMLInputElement).value),
+    map((val: string) => (val.length > 3 ? val : '')),
+    debounceTime(500),
+    distinctUntilChanged()
+  )
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log(err),
+    complete: () => console.log('end of the event'),
   });
-  console.log('End');
-});
-console.log('start sub');
-search$.subscribe({
-  next: (value) => console.log(value, 1),
-});
-search$.subscribe({
-  next: (value) => console.log(value, 2),
-});
-console.log('end sub');
